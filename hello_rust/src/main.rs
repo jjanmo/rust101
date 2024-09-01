f3fe180035a1e95cec7c28e4150f11d4877e3556
jjanmo <jjanmo07@hanmail.net>
2024-09-01 22:12:00 +0900

    feat: 샘플 가위바위보 출력 예시 구현

--- hello_rust/src/main.rs
+++ hello_rust/src/main.rs
@@ -1,3 +1,10 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("[가위, 바위, 보] 중 하나를 입력하세요");
+
+    let mut decision = String::new();
+    io::stdin().read_line(&mut decision)
+        .expect("입력 실패");
+    print!("당신의 선택: {}", decision);
 }
